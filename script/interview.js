c0a13021b21b6d1e9ba6abcfbb288d6ba0c831a9
balanced0 <alvichowdhury013@gmail.com>
2026-02-24 01:23:36 +0600

    Almost completed the functionality of the interview section

--- script/interview.js
+++ script/interview.js
@@ -4,21 +4,34 @@ const totalInterview = document.getElementById("total-interview");
 let currentTotalInterview = Number(totalInterview.innerText);
 for(let button of interviewButtons){
     button.addEventListener("click", function(){
-        interview.push(this.closest(".card"));
+        const card = this.closest(".card");
         currentTotalInterview++;
         totalInterview.innerText = currentTotalInterview;
+        const status = card.querySelector("span");
+        status.outerHTML = `<div class="badge badge-soft badge-xl badge-outline badge-success">Interview</div>`;
+        if(!interview.includes(card)){
+            interview.push(card);
+        }
     });
 }
 document.getElementById("interview-btn").addEventListener("click", function(){
     interviewBtnFocus("interview-btn");
-    const jobCards = document.getElementById("jobCardContainer");
-    jobCards.classList.add("hidden");
+    const jobCardContainer = document.getElementById("jobCardContainer");
+    const jobCards = jobCardContainer.querySelectorAll(".card");
+    for(let card of jobCards){
+        card.classList.add("hidden");
+    }
     if(interview.length === 0){
         const noJobsSection = document.getElementById("no-jobs");
         noJobsSection.classList.remove("hidden");
     }
     else{
+        const noJobsSection = document.getElementById("no-jobs");
+        noJobsSection.classList.add("hidden");
         const availableJobs = document.getElementById("available-jobs-main");
         availableJobs.innerText = `${currentTotalInterview} of ${currentTotalJobs} jobs`;
+        for(let i = 0; i < interview.length; i++){
+            interview[i].classList.remove("hidden");
+        }
     }
 });
