0a6fa16f930852bca565e2a8fc914aa74c3d1e4d
balanced0 <alvichowdhury013@gmail.com>
2026-02-23 21:57:07 +0600

    Completed the delete functionality of the site

--- script/delete.js
+++ script/delete.js
@@ -1,6 +1,18 @@
 const deleteCard = document.querySelectorAll(".trash");
+const availableJobs = document.getElementById("available-jobs");
+let currentAvailableJobs = Number(availableJobs.innerText);
+const totalJobs = document.getElementById("total-jobs");
+let currentTotalJobs = Number(totalJobs.innerText);
 for(let card of deleteCard){
     card.addEventListener("click", function(){
         this.closest(".card").remove();
+        currentAvailableJobs--;
+        availableJobs.innerText = currentAvailableJobs;
+        currentTotalJobs--;
+        totalJobs.innerText = currentTotalJobs;
+        if(currentAvailableJobs == 0){
+            const noJobsSection = document.getElementById("no-jobs");
+            noJobsSection.classList.remove("hidden");
+        }
     });
 }
