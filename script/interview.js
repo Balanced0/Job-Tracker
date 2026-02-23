f17c38b150907adc290e8485f85e55bf00709b30
balanced0 <alvichowdhury013@gmail.com>
2026-02-24 00:08:21 +0600

    Somewhat created the functionality of the interview section

---
+++ script/interview.js
@@ -0,0 +1,23 @@
+const interview = [];
+const interviewButtons = document.querySelectorAll(".interview-btn");
+const totalInterview = document.getElementById("total-interview");
+let currentTotalInterview = Number(totalInterview.innerText);
+for(let button of interviewButtons){
+    button.addEventListener("click", function(){
+        interview.push(this.closest(".card"));
+        currentTotalInterview++;
+        totalInterview.innerText = currentTotalInterview;
+    });
+}
+document.getElementById("interview-btn").addEventListener("click", function(){
+    const jobCards = document.getElementById("jobCardContainer");
+    jobCards.classList.add("hidden");
+    if(interview.length === 0){
+        const noJobsSection = document.getElementById("no-jobs");
+        noJobsSection.classList.remove("hidden");
+    }
+    else{
+        const availableJobs = document.getElementById("available-jobs-main");
+        availableJobs.innerText = `${currentTotalInterview} of ${currentTotalJobs} jobs`;
+    }
+});
